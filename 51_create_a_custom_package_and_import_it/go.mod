4699e1e5f8fb62ebc06609e02cf2555cf04aa196
Ilyas Ouzrour <ilyas.ouzrour@gmail.com>
2025-03-24 23:20:17 +0000

    Create a custom package and import it

---
+++ 51_create_a_custom_package_and_import_it/go.mod
@@ -0,0 +1,3 @@
+module github.com/ouzrourextra/go_lvl_3/51_create_a_custom_package_and_import_it
+
+go 1.23.5
---
+++ 51_create_a_custom_package_and_import_it/main.go
@@ -0,0 +1,14 @@
+// Runned by "go run ."
+
+package main
+
+import(
+	"github.com/ouzrourextra/go_lvl_3/51_create_a_custom_package_and_import_it/print"
+)
+
+func main(){
+	
+	for i:=0 ; i<10 ; i++{
+		print.Hello("Ilyas")
+	}
+}
